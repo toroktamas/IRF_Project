7e2abd6a218b04f9fe3563c8f1b13638013043b9
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-12-16 23:55:04 +0100

    Add class

---
+++ IRF_Projekt_Beadando/IRF_Projekt_Beadando/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Projekt_Beadando
+{
+    public class Data
+    {
+        public string Kod { get; set; }
+
+        public string Elado { get; set; }
+
+        public string Oldal { get; set; }
+
+        public int kerület { get; set; }
+
+        public bool Lift { get; set; }
+
+        public double SzobakSzama { get; set; }
+
+        public int Alapterulet { get; set; }
+
+        public double Ar { get; set; }
+    }
+}
