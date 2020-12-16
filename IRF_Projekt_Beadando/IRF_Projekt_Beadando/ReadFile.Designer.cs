65cdce91d1c0f04d0ac8c26247318f0a0998864b
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-12-16 23:55:36 +0100

    Refactor

--- IRF_Projekt_Beadando/IRF_Projekt_Beadando/ReadFile.Designer.cs
+++ IRF_Projekt_Beadando/IRF_Projekt_Beadando/ReadFile.Designer.cs
@@ -28,45 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.readFileWhidPath = new System.Windows.Forms.Button();
-            this.openFilePath = new System.Windows.Forms.TextBox();
             this.readWhidOpenFileDialog = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.openFileDialogPathText = new System.Windows.Forms.TextBox();
             this.openForm1 = new System.Windows.Forms.Button();
+            this.ErrorMesseges = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // readFileWhidPath
-            // 
-            this.readFileWhidPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.readFileWhidPath.Location = new System.Drawing.Point(340, 93);
-            this.readFileWhidPath.Name = "readFileWhidPath";
-            this.readFileWhidPath.Size = new System.Drawing.Size(95, 47);
-            this.readFileWhidPath.TabIndex = 0;
-            this.readFileWhidPath.Text = "Beolvaás";
-            this.readFileWhidPath.UseVisualStyleBackColor = true;
-            // 
-            // openFilePath
-            // 
-            this.openFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.openFilePath.Location = new System.Drawing.Point(24, 101);
-            this.openFilePath.Name = "openFilePath";
-            this.openFilePath.Size = new System.Drawing.Size(280, 29);
-            this.openFilePath.TabIndex = 1;
             // 
             // readWhidOpenFileDialog
             // 
             this.readWhidOpenFileDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.readWhidOpenFileDialog.Location = new System.Drawing.Point(24, 187);
+            this.readWhidOpenFileDialog.Location = new System.Drawing.Point(24, 130);
             this.readWhidOpenFileDialog.Name = "readWhidOpenFileDialog";
             this.readWhidOpenFileDialog.Size = new System.Drawing.Size(96, 47);
             this.readWhidOpenFileDialog.TabIndex = 2;
             this.readWhidOpenFileDialog.Text = "Beolvaás";
             this.readWhidOpenFileDialog.UseVisualStyleBackColor = true;
+            this.readWhidOpenFileDialog.Click += new System.EventHandler(this.readWhidOpenFileDialog_Click);
             // 
             // openFileDialog1
             // 
@@ -82,21 +63,11 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Fájl beolvasás";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(20, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(214, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Fájl elérési út megadása";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(20, 160);
+            this.label3.Location = new System.Drawing.Point(20, 94);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 24);
             this.label3.TabIndex = 5;
@@ -106,7 +77,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(136, 195);
+            this.label4.Location = new System.Drawing.Point(188, 141);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(98, 24);
             this.label4.TabIndex = 6;
@@ -115,7 +86,7 @@
             // openFileDialogPathText
             // 
             this.openFileDialogPathText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.openFileDialogPathText.Location = new System.Drawing.Point(240, 195);
+            this.openFileDialogPathText.Location = new System.Drawing.Point(303, 141);
             this.openFileDialogPathText.Name = "openFileDialogPathText";
             this.openFileDialogPathText.ReadOnly = true;
             this.openFileDialogPathText.Size = new System.Drawing.Size(195, 29);
@@ -124,7 +95,7 @@
             // openForm1
             // 
             this.openForm1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.openForm1.Location = new System.Drawing.Point(468, 240);
+            this.openForm1.Location = new System.Drawing.Point(404, 216);
             this.openForm1.Name = "openForm1";
             this.openForm1.Size = new System.Drawing.Size(94, 45);
             this.openForm1.TabIndex = 8;
@@ -132,20 +103,27 @@
             this.openForm1.UseVisualStyleBackColor = true;
             this.openForm1.Click += new System.EventHandler(this.openForm1_Click);
             // 
+            // ErrorMesseges
+            // 
+            this.ErrorMesseges.AutoSize = true;
+            this.ErrorMesseges.ForeColor = System.Drawing.Color.DarkRed;
+            this.ErrorMesseges.Location = new System.Drawing.Point(101, 234);
+            this.ErrorMesseges.Name = "ErrorMesseges";
+            this.ErrorMesseges.Size = new System.Drawing.Size(0, 13);
+            this.ErrorMesseges.TabIndex = 9;
+            // 
             // ReadFile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 298);
+            this.ClientSize = new System.Drawing.Size(524, 295);
+            this.Controls.Add(this.ErrorMesseges);
             this.Controls.Add(this.openForm1);
             this.Controls.Add(this.openFileDialogPathText);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.readWhidOpenFileDialog);
-            this.Controls.Add(this.openFilePath);
-            this.Controls.Add(this.readFileWhidPath);
             this.Name = "ReadFile";
             this.Text = "ReadFile";
             this.ResumeLayout(false);
@@ -154,16 +132,13 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button readFileWhidPath;
-        private System.Windows.Forms.TextBox openFilePath;
         private System.Windows.Forms.Button readWhidOpenFileDialog;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox openFileDialogPathText;
         private System.Windows.Forms.Button openForm1;
+        private System.Windows.Forms.Label ErrorMesseges;
     }
 }
--- IRF_Projekt_Beadando/IRF_Projekt_Beadando/ReadFile.cs
+++ IRF_Projekt_Beadando/IRF_Projekt_Beadando/ReadFile.cs
@@ -12,6 +12,8 @@ namespace IRF_Projekt_Beadando
 {
     public partial class ReadFile : Form
     {
+        public string FilePath { get; set; }
+
         public ReadFile()
         {
             InitializeComponent();
@@ -19,9 +21,34 @@ namespace IRF_Projekt_Beadando
 
         private void openForm1_Click(object sender, EventArgs e)
         {
-            Form1 form1 = new Form1();
-            form1.ShowDialog();
-            this.Close();
+            if (FilePath!= string.Empty)
+            {
+                Form1 form1 = new Form1(FilePath);
+                form1.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                ErrorMesseges.Text = "Kérem válaszon ki fájlt";
+            }
+
+        }
+
+        private void readWhidOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FilePath = openFileDialog.FileName;
+                }
+            }
+            openFileDialogPathText.Text = FilePath;
         }
     }
 }
