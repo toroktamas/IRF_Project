574d8b9f8459119735724b2e6532b5006301ed76
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-12-17 04:10:13 +0100

    Refactor

--- IRF_Projekt_Beadando/IRF_Projekt_Beadando/Form1.Designer.cs
+++ IRF_Projekt_Beadando/IRF_Projekt_Beadando/Form1.Designer.cs
@@ -29,37 +29,34 @@
         private void InitializeComponent()
         {
             this.delRowButton = new System.Windows.Forms.Button();
-            this.reloadButton = new System.Windows.Forms.Button();
             this.openWriteFileButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.importDataButton = new System.Windows.Forms.Button();
+            this.errrorMesseges = new System.Windows.Forms.Label();
+            this.keruletListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.szobaSzamListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.liftComboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.productTextBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // delRowButton
             // 
             this.delRowButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.delRowButton.Location = new System.Drawing.Point(557, 67);
+            this.delRowButton.Location = new System.Drawing.Point(381, 527);
             this.delRowButton.Name = "delRowButton";
             this.delRowButton.Size = new System.Drawing.Size(191, 62);
             this.delRowButton.TabIndex = 1;
-            this.delRowButton.Text = "Kijelült sorok törlése";
+            this.delRowButton.Text = "Kijelült elemek törlése";
             this.delRowButton.UseVisualStyleBackColor = true;
             this.delRowButton.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // reloadButton
-            // 
-            this.reloadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.reloadButton.Location = new System.Drawing.Point(557, 150);
-            this.reloadButton.Name = "reloadButton";
-            this.reloadButton.Size = new System.Drawing.Size(191, 66);
-            this.reloadButton.TabIndex = 2;
-            this.reloadButton.Text = "Újra töltés";
-            this.reloadButton.UseVisualStyleBackColor = true;
             // 
             // openWriteFileButton
             // 
             this.openWriteFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.openWriteFileButton.Location = new System.Drawing.Point(557, 372);
+            this.openWriteFileButton.Location = new System.Drawing.Point(814, 125);
             this.openWriteFileButton.Name = "openWriteFileButton";
             this.openWriteFileButton.Size = new System.Drawing.Size(191, 66);
             this.openWriteFileButton.TabIndex = 3;
@@ -80,7 +77,7 @@
             // importDataButton
             // 
             this.importDataButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.importDataButton.Location = new System.Drawing.Point(557, 282);
+            this.importDataButton.Location = new System.Drawing.Point(814, 19);
             this.importDataButton.Name = "importDataButton";
             this.importDataButton.Size = new System.Drawing.Size(191, 66);
             this.importDataButton.TabIndex = 5;
@@ -88,15 +85,93 @@
             this.importDataButton.UseVisualStyleBackColor = true;
             this.importDataButton.Click += new System.EventHandler(this.importDataButton_Click);
             // 
+            // errrorMesseges
+            // 
+            this.errrorMesseges.AutoSize = true;
+            this.errrorMesseges.ForeColor = System.Drawing.Color.DarkRed;
+            this.errrorMesseges.Location = new System.Drawing.Point(546, 19);
+            this.errrorMesseges.Name = "errrorMesseges";
+            this.errrorMesseges.Size = new System.Drawing.Size(0, 13);
+            this.errrorMesseges.TabIndex = 6;
+            // 
+            // keruletListBox1
+            // 
+            this.keruletListBox1.FormattingEnabled = true;
+            this.keruletListBox1.Location = new System.Drawing.Point(20, 125);
+            this.keruletListBox1.Name = "keruletListBox1";
+            this.keruletListBox1.Size = new System.Drawing.Size(120, 94);
+            this.keruletListBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(15, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(364, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Melyik kerületeket szeretné törölni a adatbázisból?";
+            // 
+            // szobaSzamListBox2
+            // 
+            this.szobaSzamListBox2.FormattingEnabled = true;
+            this.szobaSzamListBox2.Location = new System.Drawing.Point(19, 435);
+            this.szobaSzamListBox2.Name = "szobaSzamListBox2";
+            this.szobaSzamListBox2.Size = new System.Drawing.Size(120, 94);
+            this.szobaSzamListBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(15, 401);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(402, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Hány szobás lakásokat szeretné törölni a adatbázisból?";
+            // 
+            // liftComboBox1
+            // 
+            this.liftComboBox1.FormattingEnabled = true;
+            this.liftComboBox1.Location = new System.Drawing.Point(20, 313);
+            this.liftComboBox1.Name = "liftComboBox1";
+            this.liftComboBox1.Size = new System.Drawing.Size(121, 21);
+            this.liftComboBox1.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(16, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(352, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Azok a lakások törlése adatbázisból ahol ....... lift.";
+            // 
+            // productTextBox1
+            // 
+            this.productTextBox1.Location = new System.Drawing.Point(915, 596);
+            this.productTextBox1.Name = "productTextBox1";
+            this.productTextBox1.ReadOnly = true;
+            this.productTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.productTextBox1.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1027, 628);
+            this.Controls.Add(this.productTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.liftComboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.szobaSzamListBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.keruletListBox1);
+            this.Controls.Add(this.errrorMesseges);
             this.Controls.Add(this.importDataButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.openWriteFileButton);
-            this.Controls.Add(this.reloadButton);
             this.Controls.Add(this.delRowButton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -107,10 +182,17 @@
 
         #endregion
         private System.Windows.Forms.Button delRowButton;
-        private System.Windows.Forms.Button reloadButton;
         private System.Windows.Forms.Button openWriteFileButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button importDataButton;
+        private System.Windows.Forms.Label errrorMesseges;
+        private System.Windows.Forms.CheckedListBox keruletListBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox szobaSzamListBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox liftComboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox productTextBox1;
     }
 }
 
--- IRF_Projekt_Beadando/IRF_Projekt_Beadando/Form1.cs
+++ IRF_Projekt_Beadando/IRF_Projekt_Beadando/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,70 +16,112 @@ namespace IRF_Projekt_Beadando
 {
     public partial class Form1 : Form
     {
-        public List<Data> Data { get; set; } 
+        enum lift { Van, Nincs  }
+        public List<FlatData> FlatData { get; set; }
+            = new List<FlatData>();
 
         public Form1(string filePath)
         {
             InitializeComponent();
             FileReading(filePath);
+            PullElementValue();
         }
 
         public Form1()
         {
             InitializeComponent();
+            PullElementValue();
         }
 
+        public void PullElementValue() 
+        {
+            liftComboBox1.DisplayMember = "Text";
+            liftComboBox1.ValueMember = "Value";
+
+            liftComboBox1.DataSource = new[] {
+                new { Text = lift.Van.ToString(), Value = "true" },
+                new { Text = lift.Nincs.ToString(), Value = "false" }
+            };
+
+            keruletListBox1.DataSource = FlatData
+                .GroupBy(k => k.Kerulet)
+                .OrderBy(k => k.Key)
+                .Select(k => k.Key)
+                .ToList();
+
+            szobaSzamListBox2.DataSource = FlatData
+                .GroupBy(m => m.SzobakSzama)
+                .OrderBy(k => k.Key)
+                .Select(k => k.Key)
+                .ToList();
+
+            productTextBox1.Text = FlatData.Count.ToString();
+        }
 
         public void FileReading(string fileName)
         {
-            Data data = new Data();
-            XElement file = XElement.Load(fileName);
-
-            var ta = file;
-
-            IEnumerable<XElement> pricesByPartNos = file.Descendants("record").Descendants("Kod");
-            var a = pricesByPartNos;
-
-
-
-            //using (XmlReader reader = XmlReader.Create(fileName))
-            //{
-            //    while (reader.Read())
-            //    {
-            //        if (reader.IsStartElement())
-            //        {
-            //            switch (reader.Name)
-            //            {
-            //                case "Kod":
-            //                    // Detect this ele
-            //                    var a = reader.Value.Trim();
-            //                    break;
-
-            //            }
-            //        }
-            //    }
-            //}
+            try
+            {
+                XElement file = XElement.Load(fileName);
+                foreach (var item in file.Descendants("record"))
+                {
+                    var data = new FlatData()
+                    {
+                        Kod = item.Elements("Kod").FirstOrDefault().Value.ToString(),
+                        Elado = item.Elements("Elado").FirstOrDefault().Value.ToString(),
+                        Oldal = item.Elements("Oldal").FirstOrDefault().Value.ToString(),
+                        Kerulet = Convert.ToInt32(item.Elements("Kerulet").FirstOrDefault().Value.ToString()),
+                        Lift = (item.Elements("Lift").FirstOrDefault().Value.ToString() == "Van") ? true : false,
+                        SzobakSzama = Convert.ToDouble(item.Elements("SzobakSzama").FirstOrDefault().Value.ToString()),
+                        Alapterulet = Convert.ToInt32(item.Elements("Alapterulet").FirstOrDefault().Value.ToString()),
+                        Ar = Convert.ToDouble(item.Elements("Ar").FirstOrDefault().Value.ToString()),
+                    };
+                    FlatData.Add(data);
+                }
+            }
+            catch (Exception e)
+            {
+                errrorMesseges.Text = e.Message;
+                throw new Exception(e.Message);
+            }
+            productTextBox1.Text = FlatData.Count.ToString();
         }
 
+        //kijelölt elemek törlése a FlatData-ból.
         private void button1_Click(object sender, EventArgs e)
         {
+            var flatdata = FlatData
+                .Where(f => f.Lift == Convert.ToBoolean(liftComboBox1.SelectedValue))
+                .ToList();
 
+            foreach (var item in keruletListBox1.CheckedItems)
+            {
+                flatdata.Where(f => f.Kerulet != Convert.ToInt32(item));
+            }
+
+            foreach (var item in szobaSzamListBox2.CheckedItems)
+            {
+                flatdata.Where(f => f.SzobakSzama != Convert.ToInt32(item));
+            }
+            productTextBox1.Text = flatdata.Count.ToString();
+            FlatData = flatdata;
         }
 
-        //Vissza lépés
+        //Adat Exportálás
         private void openWriteFileButton_Click(object sender, EventArgs e)
         {
-            WriteFile file = new WriteFile();
+            WriteFile file = new WriteFile(FlatData);
             file.ShowDialog();
             this.Close();
         }
 
-        //Tovább haladás
+        //Újboli importálás
         private void importDataButton_Click(object sender, EventArgs e)
         {
-            ReadFile file = new ReadFile(); //vm
+            ReadFile file = new ReadFile();
             file.ShowDialog();
             this.Close();
+
         }
     }
 }
